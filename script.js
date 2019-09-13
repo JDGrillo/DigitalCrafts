4b0c04d7e164d17d79c2e8a3f22481016d67eaab
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-09-13 09:00:28 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function marinaraSauce (cookName="JD") {
+    let two = 2;
+    let oneFourth = "1/4";
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
+    console.log("- Heat a medium-large saucepan over medium heat.\n- Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about " + two + " minutes\n- Add " + oneFourth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.\n - Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes\n - Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+marinaraSauce("Luis");
